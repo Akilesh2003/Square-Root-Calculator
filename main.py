877bb42a0c5ab1ba94b761adfd2536362fe2569e
Akileshkumar KR <akilesh2003@gmail.com>
2020-10-31 11:21:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello! Welcome to my free Square Root calculator!")
+num = float(input('Please Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of {} is {}'.format(num ,num_sqrt))
